fb4e2c02031d34eaa69911b93b28affdb17e2b7b
Joshua T Corbin <joshua@uber.com>
2016-04-14 13:24:30 -0700

    example_server/main: fix the listening scene

--- example_server/main.go
+++ example_server/main.go
@@ -8,13 +8,18 @@ import (
 )
 
 func main() {
+	go func() {
+		gwrProto.ListenAndServe(map[string]string{
+			"http": ":4040",
+			"resp": ":4041",
+		})
+	}()
+
 	resLog := &resLogger{handler: http.DefaultServeMux}
 	reqLog := &reqLogger{handler: resLog}
 
 	gwr.AddMarshaledDataSource(reqLog)
 	gwr.AddMarshaledDataSource(resLog)
-	gwrProto.ListenAndServe(map[string]string{
-		"http": ":4040",
-		"resp": ":4041",
-	})
+
+	http.ListenAndServe(":8080", reqLog)
 }
