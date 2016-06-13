fcbb2fe1ca2fedba16ddb8fad7283122cac3a962
Joshua T Corbin <joshua@uber.com>
2016-06-13 15:09:26 -0700

    WIP: bench

--- internal/marshaled/source_test.go
+++ internal/marshaled/source_test.go
@@ -22,11 +22,14 @@ package marshaled_test
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 	"testing"
 	"text/template"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -34,6 +37,17 @@ import (
 	"github.com/uber-go/gwr/internal/marshaled"
 	"github.com/uber-go/gwr/source"
 )
+
+var (
+	debug = flag.Bool("debug", false, "Enable debug prints")
+	delay = flag.Duration("delay", 20*time.Microsecond, "How long to delay before generating a new item")
+)
+
+var words = []string{"the", "quick", "brown", "fox", "jump", "over", "the", "lazy", "hound"}
+
+type benchData struct {
+	Mess string `json:"word"`
+}
 
 type testDataSource struct {
 	watcher   source.GenericDataWatcher
@@ -97,9 +111,91 @@ func TestDataSource_Watch_activation(t *testing.T) {
 	// for mds to "drain" when it's watcher-less before moving on to next phase
 }
 
+func BenchmarkDataSource_Watch_json(b *testing.B) {
+	// TODO: parallel variant(s)
+
+	tds := &testDataSource{}
+	tds.activated = make(chan struct{}, 1)
+	mds := marshaled.NewDataSource(tds, nil)
+
+	var ps pipeSet
+	defer ps.close()
+
+	lines := make(chan string, 1000000)
+
+	var sg sync.WaitGroup
+	sg.Add(1)
+
+	go func() {
+		reconn := 0
+		require.NoError(b, ps.watch(mds, "json"))
+		sg.Done()
+		for {
+			sc := ps.scs[len(ps.scs)-1]
+			for sc.Scan() {
+				lines <- sc.Text()
+			}
+			if ps.closed {
+				break
+			}
+			reconn++
+			require.NoError(b, ps.watch(mds, "json"))
+		}
+		b.Logf("line scanner done, %d reconns\n", reconn)
+		close(lines)
+	}()
+
+	go func() {
+		sg.Wait()
+		for i := 0; i < b.N; i++ {
+			if *debug {
+				fmt.Printf("put %d\n", i)
+			}
+			word := words[i%len(words)]
+			mess := fmt.Sprintf("[%d]: %q", i, word)
+			tds.emit(benchData{Mess: mess})
+			<-time.After(*delay)
+		}
+	}()
+
+	expected := b.N
+	got := 0
+
+	// 10% more than N * delay
+	to := time.After(time.Duration(int(1.1 * float64(int(*delay)*b.N))))
+
+loop:
+	for {
+		select {
+		case line, ok := <-lines:
+			if !ok {
+				break loop
+			}
+			require.True(b, line[0] == '{' && line[len(line)-1] == '}', "looks like a json object")
+			if *debug {
+				fmt.Printf("got %q\n", line)
+			}
+			// done if we've seen expected number of lines
+			got++
+			if got >= expected {
+				break loop
+			}
+
+		case <-to:
+			break loop
+		}
+
+	}
+
+	ps.close()
+	p := 100.0 * float64(got) / float64(expected)
+	b.Logf("saw %d/%d (%.1f%%) records\n", got, expected, p)
+}
+
 type pipeSet struct {
-	rs  []*os.File
-	scs []*bufio.Scanner
+	rs     []*os.File
+	scs    []*bufio.Scanner
+	closed bool // TODO: atomic?
 }
 
 func (ps *pipeSet) watch(src source.DataSource, format string) error {
@@ -130,6 +226,7 @@ func (ps *pipeSet) closeOne(i int) error {
 }
 
 func (ps *pipeSet) close() {
+	ps.closed = true
 	for _, r := range ps.rs {
 		r.Close()
 	}
